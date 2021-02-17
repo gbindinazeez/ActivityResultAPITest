154b5664340269de72a5a597aca27bd08de56227
Olaitan Gbindinninuola <olaitan@kudabank.com>
2021-02-18 00:56:18 +0100

    Arithmetic Test for activity result API

--- app/src/main/java/com/example/activityresultapitest/MainActivity.kt
+++ app/src/main/java/com/example/activityresultapitest/MainActivity.kt
@@ -2,30 +2,46 @@ package com.example.activityresultapitest
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    val textview = findViewById<TextView>(R.id.textView)
-    val button = findViewById<Button>(R.id.button_get_result)
+    lateinit var textviewResult: TextView
 
-    val resultContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result: ActivityResult ->
+    val resultContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result: ActivityResult ->
         if (result.resultCode == Activity.RESULT_OK){
-            textview.text = "I got the result"
-        } else {
-            textview.text = "Failed to get result "
+            val result = result.data?.getIntExtra("result",0)
+            textviewResult.text = "" + result
+        }
+        if (result.resultCode == RESULT_CANCELED){
+            textviewResult.text = "Nothing selected"
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textviewResult = findViewById(R.id.text_view_result)
+        val editText1 = findViewById<EditText>(R.id.edit_text_number1)
+        val editText2 = findViewById<EditText>(R.id.edit_text_number2)
+        val button = findViewById<Button>(R.id.button_open_activity2)
         button.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            resultContract.launch(intent)
+            if (editText1.text.toString().equals("") || editText2.text.toString().equals("")){
+                Toast.makeText(this, "Please Insert numbers", Toast.LENGTH_SHORT).show()
+            } else {
+                val number1 = editText1.text.toString().toInt()
+                val number2 = editText2.text.toString().toInt()
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("number1", number1)
+                intent.putExtra("number2", number2)
+                resultContract.launch(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/activityresultapitest/ResultActivity.kt
+++ app/src/main/java/com/example/activityresultapitest/ResultActivity.kt
@@ -1,26 +1,47 @@
 package com.example.activityresultapitest
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
 
-    val button = findViewById<Button>(R.id.get_result)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        button.setOnClickListener {
-            setResult(Activity.RESULT_OK)
+        val textView = findViewById<TextView>(R.id.text_view_numbers)
+        val buttonAdd = findViewById<Button>(R.id.button_add)
+        val buttoSub = findViewById<Button>(R.id.button_subtract)
+
+        val intent = getIntent()
+        val number1 = intent.getIntExtra("number1",0)
+        val number2 = intent.getIntExtra("number2",0)
+
+        textView.text = "Numbers: " + number1 + ", " + number2
+        buttonAdd.setOnClickListener {
+            val result = number1 + number2
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("result",result)
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+
+        buttoSub.setOnClickListener {
+            val result = number1 - number2
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("result",result)
+            setResult(RESULT_OK, resultIntent)
             finish()
         }
     }
 
-    override fun onBackPressed() {
-        setResult(Activity.RESULT_CANCELED)
-        finish()
-    }
 }
